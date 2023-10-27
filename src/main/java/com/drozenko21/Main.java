1eed3d78d319b660fdd509646fec4f27266343a5
Aleksandr Drozenko <148690912+Iskander-D@users.noreply.github.com>
2023-10-27 14:31:22 +0400

    Update Main.java

--- src/main/java/com/drozenko21/Main.java
+++ src/main/java/com/drozenko21/Main.java
@@ -1,7 +1,7 @@
   package com.drozenko21;
 
 public class Main {
-  private static String msg ="Hello people!";
+  private static String msg ="Hello people!!";
     public static void main(String[] args) {
         System.out.println("msg");
     }
