930b365d4cec6f2323ecd8463e471c07b2f3781f
iskander-d <drozenko21@gmail.com>
2023-10-27 14:05:42 +0400

    added getmessage() method

--- src/main/java/com/drozenko21/Main.java
+++ src/main/java/com/drozenko21/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello people!");
     }
+    private static String getMessage() {
+        return "Hello people!"
+    }
 }
